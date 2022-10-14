8bba1c50115166a80ca0b9d9360526a678396726
@hexlet/cli <support@hexlet.io>
2022-10-14 20:44:20 +0600

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -4,39 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 // BEGIN
-    class App {
-        public static Map<String, Integer> getWordCount(String sentense) {
+class App {
+    public static Map<String, Integer> getWordCount(String sentense) {
+        Map<String, Integer> wordsCount = new HashMap<>();
 
-            Map<String, Integer> wordsCount = new HashMap<>();
-
-            if (sentense.isEmpty()) {
-                return wordsCount;
-            }
-
-            String[] words = sentense.split(" ");
-
-            for (int i = 0; i < words.length; i++) {
-                int counter = 0;
-                for (int j = 0; j < words.length; j++) {
-                    if (words[i].equals(words[j])) {
-                        counter++;
-                    }
-                }
-                wordsCount.put(words[i], counter);
-            }
+        if (sentense.isEmpty()) {
             return wordsCount;
         }
 
+        String[] words = sentense.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    counter++;
+                }
+            }
+            wordsCount.put(words[i], counter);
+        }
+        return wordsCount;
+    }
+
     public static String toString(Map<String, Integer> dictionary) {
+        if (dictionary.isEmpty()) {
+            return "{}";
+        }
+
         String values = "";
 
-        for (var item : dictionary.entrySet()) {
+        for (Map.Entry<String, Integer> item : dictionary.entrySet()) {
             values += "  " + item.getKey() + ": " + item.getValue() + "\n";
         }
 
-        return "{\n" +
-                values +
-                "\n}";
+        return "{\n" + values + "}";
     }
 }
 //END
